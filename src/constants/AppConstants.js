e149441e791fb8f4883f6413ec45774c8df6a167
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-09 10:05:49 +0000

    Update AppConstants

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -3,14 +3,17 @@ var keyMirror = require('keymirror');
 module.exports = {
 
   ActionTypes: keyMirror({
-    RECEIVE_REMOTE_ARTISTS: null,
-    RECEIVE_REMOTE_ALBUMS: null,
+    RECEIVE_LOCAL_SONGS: null,
     RECEIVE_REMOTE_SONGS: null
   }),
 
   PayloadSources: keyMirror({
     SERVER_ACTION: null,
     VIEW_ACTION: null
+  }),
+
+  Library: keyMirror({
+    UNKNOWN: null
   })
 
 };
