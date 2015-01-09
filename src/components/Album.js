537011b6c72b1f6cafdc3f25a499659028ef9395
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-09 17:14:17 +0000

    Add download interaction in Album component

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,7 +1,25 @@
 var React = require('react/addons');
+var Status = require('../constants/AppConstants').AlbumStatus;
 
 var Album = React.createClass({
+  getInitialState: function() {
+    return {
+      status: Status.NOT_DOWNLOADED
+    }
+  },
+  download: function(event) {
+    if(!confirm('Do you want to download this album')) {
+      return;
+    }
+
+    this.setState({status: Status.DOWNLOADING});
+
+    setTimeout(function() {
+      this.setState({status: Status.DOWNLOADED});
+    }.bind(this), 2500);
+  },
   render: function() {
+    var cx = React.addons.classSet;
 
     var albumCover  = this.props.model.cover ? this.props.model.cover : 'img/defaultCover.png';
 
@@ -37,12 +55,29 @@ var Album = React.createClass({
       whiteSpace: 'nowrap'
     };
 
+    var iconClasses = cx({
+      'fa': true,
+      'fa-cloud-download': (this.state.status === Status.NOT_DOWNLOADED || Status.DOWNLOADED),
+      'fa-circle-o-notch fa-spin': (this.state.status === Status.DOWNLOADING)
+    });
+
+    var iconStyle = {
+      position: 'relative',
+      float: 'right',
+      fontSize: '16px',
+      color: this.state.status === Status.DOWNLOADED ? '#0077c0' :'#ddd',
+      cursor: 'pointer'
+    };
+
     return (
       <div style={containerStyle}>
         <img src={albumCover} style={imageStyle} />
         <div style={{padding: '0 10px'}}>
           <p style={nameStyle}>{this.props.model.album || 'Unknown Album'}</p>
-          <p style={artistStyle}>{this.props.model.albumArtist || 'Unknown Artist'}</p>
+          <p style={artistStyle}>
+            {this.props.model.albumArtist || 'Unknown Artist'}
+            <i className={iconClasses} style={iconStyle} onClick={this.download}></i>
+          </p>
         </div>
       </div>
     );
