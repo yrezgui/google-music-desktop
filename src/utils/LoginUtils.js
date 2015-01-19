f8ade42295832240ad4ef316906b288a0612a30c
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-19 01:39:26 +0000

    Add LoginUtils

---
+++ src/utils/LoginUtils.js
@@ -0,0 +1,34 @@
+var AppConstants  = require('../constants/AppConstants');
+var LoginStore    = require('../stores/LoginStore');
+
+module.exports = {
+
+  getGoogleLogin: function() {
+    var prom = new Promise(function(resolve, reject) {
+      var email     = window.localStorage.getItem(AppConstants.Login.EMAIL_KEY);
+      var password  = window.localStorage.getItem(AppConstants.Login.PASSWORD_KEY);
+
+      if(!email || !password) {
+        reject(AppConstants.Errors.EMPTY_LOGIN);
+      }
+
+      resolve({email: email, password: password});
+    });
+
+    return prom;
+  },
+  saveGoogleLogin: function(email, password) {
+    if(!email || !password) {
+      return Promise.reject(AppConstants.Errors.EMPTY_LOGIN);
+    }
+
+    try {
+      window.localStorage.saveItem(AppConstants.Login.EMAIL_KEY, email);
+      window.localStorage.saveItem(AppConstants.Login.EMAIL_KEY, email);
+    } catch(e) {
+      return Promise.reject(e);
+    }
+
+    return Promise.resolve('Login saved');
+  }
+};
