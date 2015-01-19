a098cf5a0dbbe0acfa172df3bd65415bb732eda5
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-19 01:37:28 +0000

    Remove complex (and useless) CSS style

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -5,23 +5,14 @@ var Loading = React.createClass({
   render: function() {
 
     var containerStyle = {
-      height: '100%',
-      width: '100%',
-      display: 'table'
-    };
-
-    var wrapperStyle = {
-      display: 'table-cell',
-      height: '100%',
-      verticalAlign: 'middle',
-      textAlign: 'center'
+      textAlign: 'center',
+      position: 'relative',
+      top: '100px'
     };
 
     return (
       <div style={containerStyle}>
-        <div style={wrapperStyle}>
-          <img src="img/loading.gif" />
-        </div>
+        <img src="logo.svg" />
       </div>
     );
   }
