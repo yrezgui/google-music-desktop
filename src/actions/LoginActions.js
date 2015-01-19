b7a172792532fa3d1e4445e1804345b150197d3d
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-19 02:00:27 +0000

    Add saveLogin method in LoginActions Remove useless methods

--- src/actions/LoginActions.js
+++ src/actions/LoginActions.js
@@ -1,26 +1,21 @@
 var AppDispatcher = require('../dispatchers/AppDispatcher');
 var AppConstants  = require('../constants/AppConstants');
+var LoginUtils    = require('../utils/LoginUtils');
+var ApiUtils      = require('../utils/ApiUtils');
 
 var ActionTypes = AppConstants.ActionTypes;
 
 module.exports = {
 
-  executeSign: function(email, password) {
+  saveLogin: function(email, password) {
     AppDispatcher.handleServerAction({
-      type: ActionTypes.EXECUTE_SIGNIN,
+      type: ActionTypes.SAVE_LOGIN,
       email: email
     });
-  },
-  executeSignInSuccess: function(error) {
-    AppDispatcher.handleServerAction({
-      type: ActionTypes.EXECUTE_SIGNIN_SUCCESS,
-      error: error
-    });
-  },
-  executeSignInFail: function(email, password) {
-    AppDispatcher.handleServerAction({
-      type: ActionTypes.EXECUTE_SIGNIN_FAIL,
-      error: error
-    });
+
+    LoginUtils.saveGoogleLogin(email, password)
+      .then(LoginUtils.getGoogleLogin())
+      .then(ApiUtils.executeSignIn)
+      .catch(ApiUtils.onEmptyLogin);
   }
 };
