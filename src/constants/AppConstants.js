0ab24c49523722dbebe5219ff67d3856b8316693
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-19 01:38:06 +0000

    Add more ActionTypes, LoginStatus, Login store keys, Folders names, Errors types

--- src/constants/AppConstants.js
+++ src/constants/AppConstants.js
@@ -3,8 +3,16 @@ var keyMirror = require('keymirror');
 module.exports = {
 
   ActionTypes: keyMirror({
-    RECEIVE_LOCAL_SONGS: null,
-    RECEIVE_REMOTE_SONGS: null
+    FETCH_SONGS: null,
+    RECEIVE_SONGS_SUCCESS: null,
+    RECEIVE_SONGS_FAIL: null,
+    DOWNLOAD_ALBUM: null,
+    START_DOWNLOADING_SONG: null,
+    FINISH_DOWNLOADING_SONG: null,
+    EXECUTE_SIGNIN: null,
+    EXECUTE_SIGNIN_SUCCESS: null,
+    EXECUTE_SIGNIN_FAIL: null,
+    EXECUTE_SIGNIN_LOGIN_EMPTY: null
   }),
 
   PayloadSources: keyMirror({
@@ -20,6 +28,31 @@ module.exports = {
     NOT_DOWNLOADED: null,
     DOWNLOADING: null,
     DOWNLOADED: null
+  }),
+
+  LoginStatus: keyMirror({
+    LOADING: null,
+    SIGNED_IN: null,
+    SIGNED_OUT: null
+  }),
+
+  Login: {
+    EMAIL_KEY: 'googleEmail',
+    PASSWORD_KEY: 'googlePassword',
+  },
+
+  Folders: {
+    HOME: process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE,
+    DOWNLOADS: '/Downloads'
+  },
+
+  Errors: keyMirror({
+    EMPTY_LOGIN: null,
+    WRONG_LOGIN: null
+  }),
+
+  Channels: keyMirror({
+    MAIN: null
   })
 
 };
