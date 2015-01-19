f52e1e2509b812129bdc16befac9b033d9c0d240
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-19 01:42:01 +0000

    Add more ServerActions related to ApiUtils

--- src/actions/ServerActions.js
+++ src/actions/ServerActions.js
@@ -4,8 +4,48 @@ var AppConstants  = require('../constants/AppConstants');
 var ActionTypes = AppConstants.ActionTypes;
 
 module.exports = {
-
-  receiveLocalSongs: function(rawSongs) {
+  executeSignIn: function(email, password) {
+    AppDispatcher.handleServerAction({
+      type: ActionTypes.EXECUTE_SIGNIN,
+      email: email
+    });
+  },
+  onSignInSuccess: function(error) {
+    AppDispatcher.handleServerAction({
+      type: ActionTypes.EXECUTE_SIGNIN_SUCCESS,
+      error: error
+    });
+  },
+  onSignInFail: function(email, password) {
+    AppDispatcher.handleServerAction({
+      type: ActionTypes.EXECUTE_SIGNIN_FAIL,
+      error: error
+    });
+  },
+  onEmptyLogin: function(error) {
+    AppDispatcher.handleServerAction({
+      type: ActionTypes.EXECUTE_SIGNIN_LOGIN_EMPTY,
+      error: error
+    });
+  },
+  fetchSongs: function() {
+    AppDispatcher.handleServerAction({
+      type: ActionTypes.FETCH_SONGS
+    });
+  },
+  receiveSongs: function(rawSongs) {
+    AppDispatcher.handleServerAction({
+      type: ActionTypes.RECEIVE_SONGS,
+      rawSongs: rawSongs
+    });
+  },
+  downloadAlbum: function(album) {
+    AppDispatcher.handleViewAction({
+      type: ActionTypes.DOWNLOAD_ALBUM,
+      album: album
+    });
+  },
+  downloadSong: function(path, song) {
     AppDispatcher.handleServerAction({
       type: ActionTypes.RECEIVE_LOCAL_SONGS,
       rawSongs: rawSongs
