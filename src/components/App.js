eccc1b9493f08a0b281fbc9981e02a1173e70fb6
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-19 01:44:02 +0000

    Update App main component whenever the user is connected or not

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,49 @@
-var React   = require('react/addons');
-var Loading = require('./Loading');
-var Header  = require('./Header');
-var Library = require('./Library');
+var React         = require('react/addons');
+var Loading       = require('./Loading');
+var Login         = require('./Login');
+var Header        = require('./Header');
+var Library       = require('./Library');
+var LoginStore    = require('../stores/LoginStore');
+var AppConstants  = require('../constants/AppConstants');
 
 var App = React.createClass({
-
+  getInitialState: function() {
+    return {
+      status: LoginStore.getStatus()
+    };
+  },
+  componentDidMount: function() {
+    LoginStore.addChangeListener(this._onChange);
+  },
+  componentWillUnmount: function() {
+    LoginStore.removeChangeListener(this._onChange);
+  },
+  _onChange: function() {
+    this.setState({
+      status: LoginStore.getStatus()
+    });
+  },
   render: function() {
-    return (
-      <div>
-        <Header />
-        <Library />
-      </div>
-    );
+    var page;
+
+    if(this.state.status === AppConstants.LoginStatus.SIGNED_IN) {
+      return (
+        <div>
+          <Header />
+          <Library />
+        </div>
+      );
+    }
+
+    if(this.state.status === AppConstants.LoginStatus.SIGNED_OUT) {
+      return (
+        <div>
+          <Login />
+        </div>
+      );
+    }
+
+    return <Loading />;
   }
 });
 
