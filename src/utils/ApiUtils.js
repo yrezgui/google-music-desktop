f4081437f8b39c1ee6fa8fd98d1d23a614c18db6
Yacine Rezgui <rezgui.y@gmail.com>
2015-01-19 01:41:47 +0000

    Big update of ApiUtils related to the migration to Atom-Shell

--- src/utils/ApiUtils.js
+++ src/utils/ApiUtils.js
@@ -1,18 +1,62 @@
-var request       = require('superagent');
 var AppConstants  = require('../constants/AppConstants');
+var ActionTypes   = AppConstants.ActionTypes;
 var ServerActions = require('../actions/ServerActions');
+var LoginStore    = require('../stores/LoginStore');
+var ipc           = window.requireNode('ipc');
 
-module.exports = {
+var ApiUtils = {
+  fetchSongs: function() {
+    ServerActions.fetchSongs();
 
-  fetchLocalSongs: function() {
-    request
-      .get('data.json')
-      .end(function(response){
-        if (!response.body.data && !response.body.data.items) {
-          return;
-        }
+    ipc.send(AppConstants.Channels.MAIN, {
+      action: ActionTypes.FETCH_SONGS
+    });
+  },
+  executeSignIn: function(config) {
+    ServerActions.executeSignIn(config.email);
 
-        ServerActions.receiveLocalSongs(response.body.data.items);
-      });
+    ipc.send(AppConstants.Channels.MAIN, {
+      action: ActionTypes.EXECUTE_SIGNIN,
+      config: config
+    });
+  },
+  onSignInSuccess: function(response) {
+    ServerActions.onSignInSuccess(response.data);
+    console.log('SUCCESS', response);
+
+    ApiUtils.fetchSongs();
+  },
+  onSignInFail: function(response) {
+    ServerActions.onSignInSuccess(response.error);
+    console.log('ERROR', response);
+  },
+  onEmptyLogin: function(error) {
+    ServerActions.onEmptyLogin(error);
+    console.log('ERROR', error);
+  },
+  onReceiveSongs: function(response) {
+    ServerActions.receiveSongs(response.library.data.items);
   }
 };
+
+
+ipc.on(AppConstants.Channels.MAIN, function(response) {
+
+  switch(response.action) {
+
+    case ActionTypes.EXECUTE_SIGNIN_SUCCESS:
+      ApiUtils.onSignInSuccess(response);
+      break;
+
+    case ActionTypes.EXECUTE_SIGNIN_FAIL:
+      ApiUtils.onSignInFail(response);
+      break;
+
+    case ActionTypes.RECEIVE_SONGS_SUCCESS:
+      ApiUtils.onReceiveSongs(response);
+      break;
+  }
+});
+
+
+module.exports = ApiUtils;
